a5fb06e9eea3ce1b4b216b0a18638d2413f1bf8e
Artur Rossi Junior <artur.15.rossi@gmail.com>
2021-10-06 18:15:04 -0300

    Primeiro commit

---
+++ Projeto-Series/Classes/Serie.cs
@@ -0,0 +1,54 @@
+﻿using DIO.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Descricao = descricao;
+            this.Titulo = titulo;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Descrição: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Genero + Environment.NewLine;
+            retorno += "Ano de inicio: " + this.Genero + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
